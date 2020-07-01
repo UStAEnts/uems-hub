cf4c60b7686badfb24eca922ee701381866a8f93
Paul Lancaster <paul@lancasterzone.com>
2020-07-01 17:30:07 +0100

    Working test-run.sh script

--- test-run.sh
+++ test-run.sh
@@ -1,4 +1,9 @@
-sudo docker-compose -f docker-compose.test.yml -p CI build
-sudo docker-compose -f docker-compose.test.yml -p CI up -d
+# Creates the uems environment then runs the integration tests.
+# Returns error code 0 if successful or 1 if a test failed.
+sudo docker-compose -f docker-compose.test.yml -p ci up -d
 sudo docker logs -f ci_sut_1
-docker wait ci_sut_1 || return 1
+RESULT=$(sudo docker wait ci_sut_1)
+
+sudo docker-compose -f docker-compose.test.yml -p ci down
+
+return $RESULT
