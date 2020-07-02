14d1e5a62f344189552884a35158a367fd35ad35
Paul Lancaster <paul@lancasterzone.com>
2020-07-02 11:20:15 +0100

    integration tests passing

--- integration-tests/test_events.py
+++ integration-tests/test_events.py
@@ -4,14 +4,25 @@ import time
 
 EVENTS_BASE_URL = "http://gateway:15450/events"
 
-def test_query_event_name():
+def make_query(params):
     try:
-        res = requests.get(EVENTS_BASE_URL, params={'access_token': 1, 'name': 'The Bop'})
+        res = requests.get(EVENTS_BASE_URL, params=params)
     except RuntimeError:
         assert False
-
     assert (res.status_code == 200)
-    content = res.json()
-    print(content)
+    return res.json()
+
+def test_query_event_name():
+    content = make_query(params={'access_token': 1, 'name': 'The Bop'})
     assert (len(content) == 1)
     assert (content[0]['name'] == 'The Bop')
+
+def test_query_all():
+    content = make_query(params={'access_token': 1})
+    assert (len(content) == 2)
+    if content[0]['name'] == 'The Bop':
+        assert content[1]['name'] == 'Sinners'
+    elif content[0]['name'] == 'Sinners':
+        assert content[1]['name'] == 'The Bop'
+    else:
+        pytest.fail("Unrecognised event name returned")
