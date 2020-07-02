9f0d8e486166364e23325c48716a490fc3f48c20
Paul Lancaster <paul@lancasterzone.com>
2020-07-02 21:21:21 +0100

    Event addition working

--- integration-tests/test_events.py
+++ integration-tests/test_events.py
@@ -3,6 +3,8 @@ import requests
 import datetime
 import json
 
+from datetime import timezone
+
 EVENTS_BASE_URL = "http://gateway:15450/events"
 
 def make_query(params):
@@ -32,7 +34,6 @@ def json_datetime(year=0, month=0, day=0, hour=0, minute=0):
         }
     )
 
-
 def test_query_event_name():
     content = make_query(params={'access_token': 1, 'name': 'The Bop'})
     assert (len(content) == 1)
@@ -48,19 +49,24 @@ def test_query_all():
     else:
         pytest.fail("Unrecognised event name returned")
 
-def test_add_event():
-    start_date = json_datetime(2020, 7, 1, 13, 30)
-    end_date = json_datetime(2020, 7, 1, 17, 0)
+def test_add_get_event():
+    start_date = datetime.datetime(2020, 7, 1, 13, 30)
+    end_date = datetime.datetime(2020, 7, 1, 17, 0)
+
     venue = 'The Stage'
 
-    content = make_add_event(params={
+    event_name = 'TestEvent'
+
+    make_add_event(params={
         'access_token' : 1,
         'name': 'TestEvent',
-        'start_date': start_date,
-        'end_date': end_date,
+        'start_date': start_date.replace(tzinfo=timezone.utc).timestamp(),
+        'end_date': end_date.replace(tzinfo=timezone.utc).timestamp(),
         'venue': venue
         }
     )
 
-    assert (len(content) == 1)
-    assert ('event_id' in content)
+    res = make_query(params={'access_token': 1, 'name': event_name, 'venue': venue})
+    assert (len(res) == 1)
+    assert (res[0]['name'] == event_name)
+    assert (res[0]['venue'] == venue)
