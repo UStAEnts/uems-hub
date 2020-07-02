66e061cb9e78250522a70240deb589c72c56920c
Paul Lancaster <paul@lancasterzone.com>
2020-07-02 15:51:00 +0100

    Add event simple test (note addevent not impl on dionysus

--- integration-tests/test_events.py
+++ integration-tests/test_events.py
@@ -1,6 +1,7 @@
 import pytest
 import requests
-import time
+import datetime
+import json
 
 EVENTS_BASE_URL = "http://gateway:15450/events"
 
@@ -11,6 +12,26 @@ def make_query(params):
         assert False
     assert (res.status_code == 200)
     return res.json()
+
+def make_add_event(params):
+    try:
+        res = requests.post(EVENTS_BASE_URL, json=params)
+    except RuntimeError:
+        assert False
+    assert (res.status_code == 200)
+    return res.json()
+
+def json_datetime(year=0, month=0, day=0, hour=0, minute=0):
+    return json.dumps(
+        {
+            'year': year,
+            'month': month,
+            'day': day,
+            'hour': hour,
+            'minute': minute
+        }
+    )
+
 
 def test_query_event_name():
     content = make_query(params={'access_token': 1, 'name': 'The Bop'})
@@ -26,3 +47,20 @@ def test_query_all():
         assert content[1]['name'] == 'The Bop'
     else:
         pytest.fail("Unrecognised event name returned")
+
+def test_add_event():
+    start_date = json_datetime(2020, 7, 1, 13, 30)
+    end_date = json_datetime(2020, 7, 1, 17, 0)
+    venue = 'The Stage'
+
+    content = make_add_event(params={
+        'access_token' : 1,
+        'name': 'TestEvent',
+        'start_date': start_date,
+        'end_date': end_date,
+        'venue': venue
+        }
+    )
+
+    assert (len(content) == 1)
+    assert ('event_id' in content)
