635bd4e1d2d339b05160136b04ac2bb0855a1439
Paul Lancaster <paul@lancasterzone.com>
2020-07-08 19:47:15 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-(cd uems-event-micro-dionysus/; tsc)
-(cd uems-gateway/; tsc)
+(cd uems-event-micro-dionysus/; npm install; npm run-script build;)
+(cd uems-gateway/; npm install; npm run-script build;)
 (cd uems-frontend-themis/; npm install; npm run-script build;)
