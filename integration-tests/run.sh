151790b21629248f9dee00d729f2806ef137cf62
Paul Lancaster <paul@lancasterzone.com>
2020-07-13 00:00:25 +0100

    Updated tests

--- integration-tests/run.sh
+++ integration-tests/run.sh
@@ -1,2 +1,2 @@
-sleep 5
+sleep 5 # Wait to allow the system to startup completely - There must be a better way to do this.
 pytest -q --tb=line --capture=tee-sys
--- integration-tests/test_events.py
+++ integration-tests/test_events.py
@@ -6,39 +6,57 @@ import dateutil.parser
 
 from datetime import timezone
 
-EVENTS_BASE_URL = "http://gateway:15450/events"
+EVENTS_GET_URL = "http://gateway:15450/events"
 
-def send_get_query(params):
+def send_get_events(name=None, start_before=None, start_after=None, end_before=None, end_after=None):
+    params = {
+        'access_token': 1
+    }
+    if name is not None:
+        params['name'] = name
+
+    if start_before is not None:
+        params['startbefore'] = start_before
+
+    if start_after is not None:
+        params['startafter'] = start_after
+
+    if end_before is not None:
+        params['endbefore'] = end_before
+                    
+    if end_after is not None:
+        params['endafter'] = end_after
+
     try:
-        res = requests.get(EVENTS_BASE_URL, params=params)
+        res = requests.get(EVENTS_GET_URL, params=params)
     except RuntimeError:
         assert False
     assert (res.status_code == 200)
     return res.json()
 
-def send_add_event(params):
-    try:
-        res = requests.post(EVENTS_BASE_URL, json=params)
-    except RuntimeError:
-        assert False
-    assert (res.status_code == 200)
-    return res.json()
+# def send_add_event(params):
+#     try:
+#         res = requests.post(EVENTS_BASE_URL, json=params)
+#     except RuntimeError:
+#         assert False
+#     assert (res.status_code == 200)
+#     return res.json()
 
-def send_update_query(params):
-    try:
-        res = requests.patch(EVENTS_BASE_URL, json=params)
-    except RuntimeError:
-        assert False
-    assert (res.status_code == 200)
-    return res.json()
+# def send_update_query(params):
+#     try:
+#         res = requests.patch(EVENTS_BASE_URL, json=params)
+#     except RuntimeError:
+#         assert False
+#     assert (res.status_code == 200)
+#     return res.json()
 
-def send_delete_query(params):
-    try:
-        res = requests.delete(EVENTS_BASE_URL, json=params)
-    except RuntimeError:
-        assert False
-    assert (res.status_code == 200)
-    return res.json()
+# def send_delete_query(params):
+#     try:
+#         res = requests.delete(EVENTS_BASE_URL, json=params)
+#     except RuntimeError:
+#         assert False
+#     assert (res.status_code == 200)
+#     return res.json()
 
 def json_datetime(year=0, month=0, day=0, hour=0, minute=0):
     return json.dumps(
@@ -53,13 +71,13 @@ def json_datetime(year=0, month=0, day=0, hour=0, minute=0):
 
 @pytest.mark.timeout(5)
 def test_query_event_name():
-    content = send_get_query(params={'access_token': 1, 'name': 'The Bop'})
+    content = send_get_events(name='The Bop')
     assert (len(content) == 1)
     assert (content[0]['name'] == 'The Bop')
 
 @pytest.mark.timeout(5)
 def test_query_all():
-    content = send_get_query(params={'access_token': 1})
+    content = send_get_events()
     assert (len(content) == 2)
     if content[0]['name'] == 'The Bop':
         assert content[1]['name'] == 'Sinners'
@@ -68,78 +86,78 @@ def test_query_all():
     else:
         pytest.fail("Unrecognised event name returned")
 
-@pytest.mark.timeout(5)
-def test_add_get_event():
-    start_date = datetime.datetime(2020, 7, 1, 13, 30)
-    end_date = datetime.datetime(2020, 7, 1, 17, 0)
+# @pytest.mark.timeout(5)
+# def test_add_get_event():
+#     start_date = datetime.datetime(2020, 7, 1, 13, 30)
+#     end_date = datetime.datetime(2020, 7, 1, 17, 0)
 
-    venue = 'The Stage'
+#     venue = 'The Stage'
 
-    event_name = 'TestEvent'
+#     event_name = 'TestEvent'
 
-    send_add_event(params={
-        'access_token' : 1,
-        'name': 'TestEvent',
-        'start_date': start_date.replace(tzinfo=timezone.utc).timestamp(),
-        'end_date': end_date.replace(tzinfo=timezone.utc).timestamp(),
-        'venue': venue
-        }
-    )
+#     send_add_event(params={
+#         'access_token' : 1,
+#         'name': 'TestEvent',
+#         'start_date': start_date.replace(tzinfo=timezone.utc).timestamp(),
+#         'end_date': end_date.replace(tzinfo=timezone.utc).timestamp(),
+#         'venue': venue
+#         }
+#     )
 
-    res = send_get_query(params={'access_token': 1, 'name': event_name, 'venue': venue})
-    assert (len(res) == 1)
-    assert (res[0]['name'] == event_name)
-    assert (res[0]['venue'] == venue)
+#     res = send_get_query(params={'access_token': 1, 'name': event_name, 'venue': venue})
+#     assert (len(res) == 1)
+#     assert (res[0]['name'] == event_name)
+#     assert (res[0]['venue'] == venue)
 
-@pytest.mark.timeout(5)
-def test_get_modify_event_name():
-    content = send_get_query(params={'access_token': 1, 'name': 'The Bop'})
-    assert (len(content) == 1)
-    assert (content[0]['name'] == 'The Bop')
+# @pytest.mark.timeout(5)
+# def test_get_modify_event_name():
+#     content = send_get_query(params={'access_token': 1, 'name': 'The Bop'})
+#     assert (len(content) == 1)
+#     assert (content[0]['name'] == 'The Bop')
 
-    start_date = dateutil.parser.parse(content[0]['start_date'])
-    end_date = dateutil.parser.parse(content[0]['end_date'])
-    venue = content[0]['venue']
+#     start_date = dateutil.parser.parse(content[0]['start_date'])
+#     end_date = dateutil.parser.parse(content[0]['end_date'])
+#     venue = content[0]['venue']
 
-    id = content[0]['_id']
+#     id = content[0]['_id']
 
-    # Update event name from 'The Bop' to 'The Wop'
-    send_update_query(params= {
-        'access_token': 1, 
-        'event_id': id, 
-        'name': 'The Wop',
-        'start_date': start_date.replace(tzinfo=timezone.utc).timestamp(),
-        'end_date': end_date.replace(tzinfo=timezone.utc).timestamp(),
-        'venue': content[0]['venue']
-        })
+#     # Update event name from 'The Bop' to 'The Wop'
+#     send_update_query(params= {
+#         'access_token': 1, 
+#         'event_id': id, 
+#         'name': 'The Wop',
+#         'start_date': start_date.replace(tzinfo=timezone.utc).timestamp(),
+#         'end_date': end_date.replace(tzinfo=timezone.utc).timestamp(),
+#         'venue': content[0]['venue']
+#         })
 
-    new_res = send_get_query(params={'access_token': 1, 'name': 'The Wop'})
-    assert (len(new_res) == 1)
-    assert (new_res[0]['name'] == 'The Wop')
-    assert (dateutil.parser.parse(new_res[0]['start_date']) == start_date)
-    assert (dateutil.parser.parse(new_res[0]['end_date']) == end_date)
-    assert (new_res[0]['venue'] == venue)
+#     new_res = send_get_query(params={'access_token': 1, 'name': 'The Wop'})
+#     assert (len(new_res) == 1)
+#     assert (new_res[0]['name'] == 'The Wop')
+#     assert (dateutil.parser.parse(new_res[0]['start_date']) == start_date)
+#     assert (dateutil.parser.parse(new_res[0]['end_date']) == end_date)
+#     assert (new_res[0]['venue'] == venue)
 
-@pytest.mark.timeout(5)
-def test_add_get_delete_event():
-    event_name = 'TESTDELETEEVENT'
-    send_add_event(params={
-        'access_token' : 1,
-        'name': event_name,
-        'start_date': datetime.datetime(2020, 7, 1, 13, 30).replace(tzinfo=timezone.utc).timestamp(),
-        'end_date': datetime.datetime(2020, 7, 1, 17, 0).replace(tzinfo=timezone.utc).timestamp(),
-        'venue': 'The Stage'
-        }
-    )
+# @pytest.mark.timeout(5)
+# def test_add_get_delete_event():
+#     event_name = 'TESTDELETEEVENT'
+#     send_add_event(params={
+#         'access_token' : 1,
+#         'name': event_name,
+#         'start_date': datetime.datetime(2020, 7, 1, 13, 30).replace(tzinfo=timezone.utc).timestamp(),
+#         'end_date': datetime.datetime(2020, 7, 1, 17, 0).replace(tzinfo=timezone.utc).timestamp(),
+#         'venue': 'The Stage'
+#         }
+#     )
 
-    content = send_get_query(params={'access_token': 1, 'name': 'TESTDELETEEVENT'})
-    assert (len(content) == 1)
-    assert (content[0]['name'] == event_name)
+#     content = send_get_query(params={'access_token': 1, 'name': 'TESTDELETEEVENT'})
+#     assert (len(content) == 1)
+#     assert (content[0]['name'] == event_name)
 
-    id = content[0]['_id']
+#     id = content[0]['_id']
 
-    send_delete_query(params={'access_token': 1, 'event_id': id})
+#     send_delete_query(params={'access_token': 1, 'event_id': id})
 
-    res = send_get_query(params={'access_token': 1, 'name': event_name})
-    assert (len(res) == 0)
+#     res = send_get_query(params={'access_token': 1, 'name': event_name})
+#     assert (len(res) == 0)
     
