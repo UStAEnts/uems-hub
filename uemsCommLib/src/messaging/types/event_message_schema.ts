4d187d18010ee123210baff0d2bc16b91dd75090
Paul Lancaster <paul@lancasterzone.com>
2020-07-18 10:32:47 +0100

    Replaced String usages with string

--- uemsCommLib/src/messaging/types/event_message_schema.ts
+++ uemsCommLib/src/messaging/types/event_message_schema.ts
@@ -3,16 +3,16 @@ export enum MsgIntention {
 }
 
 // A string is used because the work of parsing etc. of numbers is deferred to the backend microservices.
-export type UemsDateTime = String;
+export type UemsDateTime = string;
 
 export type CreateEventMsg = {
     msg_id: Number,
     status: Number,
     msg_intention: MsgIntention.CREATE,
-    event_name: String,
+    event_name: string,
     event_start_date: UemsDateTime,
     event_end_date: UemsDateTime,
-    venue_ids: String[],
+    venue_ids: string[],
     predicted_attendance: Number
 };
 
@@ -20,13 +20,13 @@ export type ReadEventMsg = {
     msg_id: Number,
     status: Number,
     msg_intention: MsgIntention.READ,
-    event_id?: String,
-    event_name?: String,
+    event_id?: string,
+    event_name?: string,
     event_start_date_range_begin?: UemsDateTime,
     event_start_date_range_end?: UemsDateTime,
     event_end_date_range_begin?: UemsDateTime,
     event_end_date_range_end?: UemsDateTime,
-    venue_ids?: String[],
+    venue_ids?: string[],
     attendance?: Number
 };
 
@@ -34,11 +34,11 @@ export type UpdateEventMsg = {
     msg_id: Number,
     status: Number,
     msg_intention: MsgIntention.UPDATE,
-    event_id: String,
-    event_name?: String,
+    event_id: string,
+    event_name?: string,
     event_start_date?: UemsDateTime,
     event_end_date?: UemsDateTime,
-    venue_ids?: String[],
+    venue_ids?: string[],
     predicted_attendance?: Number
 };
 
@@ -46,7 +46,7 @@ export type DeleteEventMsg = {
     msg_id: Number,
     status: Number,
     msg_intention: MsgIntention.DELETE,
-    event_id: String,
+    event_id: string,
 };
 
 export type EventMsg = CreateEventMsg | ReadEventMsg | UpdateEventMsg | DeleteEventMsg;
