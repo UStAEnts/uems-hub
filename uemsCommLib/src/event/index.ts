91790842b92241ef5b839b35c5ac5423dcf75900
Ryan Delaney <vitineth@gmail.com>
2021-01-01 21:24:45 +0000

    Adding new event schema
    This adds mroe query and update functions for searching. This also defines everything as required in the api spec as far as I can tell

---
+++ uemsCommLib/src/event/index.ts
@@ -0,0 +1,33 @@
+/**
+* Restructures ./event into a format to be directly re-exported by src/index.ts
+*/
+import { EventValidators } from "./EventValidators";
+
+/**
+* Re-exports of types and functions in {@link EventValidators} relating to Event messages
+*/
+export namespace EventMessage {
+    export type CreateEventMessage = EventValidators.EventCreateSchema;
+    export type UpdateEventMessage = EventValidators.EventUpdateSchema;
+    export type DeleteEventMessage = EventValidators.EventDeleteSchema;
+    export type ReadEventMessage = EventValidators.EventReadSchema;
+    export type EventMessage = EventValidators.EventMessage;
+    export const messageToJSON = EventValidators.messageToJSON;
+}
+
+/**
+* Re-exports of types and functions in {@link EventValidators} relating to the event messages. This overrides
+* exported types with their correct result types for accuracy.
+*/
+export namespace EventResponse {
+    export type InternalEvent = EventValidators.EventRepresentation;
+    export type EventReadResponseMessage = Omit<EventValidators.EventResponseSchema, 'result'> & {
+        result: EventValidators.EventRepresentation[],
+    };
+    export type EventResponseMessage = Omit<EventValidators.EventResponseSchema, 'result'> & {
+        result: string[],
+    };
+}
+
+export const EventMessageValidator = EventValidators.EventMessageValidator;
+export const EventResponseValidator = EventValidators.EventResponseValidator;
