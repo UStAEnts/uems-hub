2fa3aa54e3aa0a4271af6fc850189395cf9faa55
Ryan Delaney <vitineth@gmail.com>
2022-07-06 17:18:49 +0100

    Finishing state migration

--- uemsCommLib/src/state/index.ts
+++ uemsCommLib/src/state/index.ts
@@ -1,18 +1,17 @@
 /**
- * Restructures ./state into a format to be directly re-exported by src/index.ts
+ * Restructures ./state into a format to be re-exported by src/index.ts
  */
-import { StateValidators } from "./StateValidators";
+import { StateValidators } from './StateValidators';
 
 /**
- * Re-exports of types and functions in {@link StateValidators} relating to State messages
+ * Re-exports of types and functions in {@link StateValidators} relating to state messages
  */
 export namespace StateMessage {
-    export type CreateStateMessage = StateValidators.StateCreateSchema;
-    export type UpdateStateMessage = StateValidators.StateUpdateSchema;
-    export type DeleteStateMessage = StateValidators.StateDeleteSchema;
-    export type ReadStateMessage = StateValidators.StateReadSchema;
+    export type ReadStateMessage = StateValidators.StateRead;
+    export type CreateStateMessage = StateValidators.StateCreate;
+    export type UpdateStateMessage = StateValidators.StateUpdate;
+    export type DeleteStateMessage = StateValidators.StateDelete;
     export type StateMessage = StateValidators.StateMessage;
-    export const messageToJSON = StateValidators.messageToJSON;
 }
 
 /**
@@ -21,12 +20,9 @@ export namespace StateMessage {
  */
 export namespace StateResponse {
     export type InternalState = StateValidators.StateRepresentation;
-    export type StateReadResponseMessage = Omit<StateValidators.StateResponseSchema, 'result'> & {
-        result: StateValidators.StateRepresentation[],
-    };
-    export type StateResponseMessage = Omit<StateValidators.StateResponseSchema, 'result'> & {
-        result: string[],
-    };
+    export type StateReadResponseMessage = StateValidators.StateReadResponse;
+    export type StateModifyResponseMessage = StateValidators.StateModifyResponse;
+    export type StateResponseMessage = StateReadResponseMessage | StateModifyResponseMessage;
 }
 
 export const StateMessageValidator = StateValidators.StateMessageValidator;
