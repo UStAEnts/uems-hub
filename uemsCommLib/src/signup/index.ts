2db0a7469ca0932bf3dc115095eeb65e48ca2f48
Ryan Delaney <vitineth@gmail.com>
2022-07-06 17:19:15 +0100

    Finishing signup migration

--- uemsCommLib/src/signup/index.ts
+++ uemsCommLib/src/signup/index.ts
@@ -1,36 +1,29 @@
 /**
-* Restructures ./signup into a format to be directly re-exported by src/index.ts
-*/
-import { SignupValidators } from "./SignupValidators";
+ * Restructures ./signup into a format to be re-exported by src/index.ts
+ */
+import { SignupValidators } from './SignupValidators';
 
 /**
-* Re-exports of types and functions in {@link SignupValidators} relating to Signup messages
-*/
+ * Re-exports of types and functions in {@link SignupValidators} relating to signup messages
+ */
 export namespace SignupMessage {
-    export type CreateSignupMessage = SignupValidators.SignupCreateSchema;
-    export type UpdateSignupMessage = SignupValidators.SignupUpdateSchema;
-    export type DeleteSignupMessage = SignupValidators.SignupDeleteSchema;
-    export type ReadSignupMessage = SignupValidators.SignupReadSchema;
+    export type ReadSignupMessage = SignupValidators.SignupRead;
+    export type CreateSignupMessage = SignupValidators.SignupCreate;
+    export type UpdateSignupMessage = SignupValidators.SignupUpdate;
+    export type DeleteSignupMessage = SignupValidators.SignupDelete;
     export type SignupMessage = SignupValidators.SignupMessage;
-    export const messageToJSON = SignupValidators.messageToJSON;
 }
 
 /**
-* Re-exports of types and functions in {@link SignupValidators} relating to the signup messages. This overrides
-* exported types with their correct result types for accuracy.
-*/
+ * Re-exports of types and functions in {@link SignupValidators} relating to the signup messages. This overrides
+ * exported types with their correct result types for accuracy.
+ */
 export namespace SignupResponse {
-    export type ShallowInternalSignup = SignupValidators.ShallowSignupRepresentation;
     export type InternalSignup = SignupValidators.SignupRepresentation;
-    export type SignupReadResponseMessage = Omit<SignupValidators.SignupResponseSchema, 'result'> & {
-        result: SignupValidators.SignupRepresentation[],
-    };
-    export type SignupServiceReadResponseMessage = Omit<SignupValidators.SignupResponseSchema, 'result'> & {
-        result: SignupValidators.ShallowSignupRepresentation[],
-    }
-    export type SignupResponseMessage = Omit<SignupValidators.SignupResponseSchema, 'result'> & {
-        result: string[],
-    };
+    export type SignupServiceReadResponseMessage = SignupValidators.SignupShallowRepresentation;
+    export type SignupReadResponseMessage = SignupValidators.SignupReadResponse;
+    export type SignupModifyResponseMessage = SignupValidators.SignupModifyResponse;
+    export type SignupResponseMessage = SignupReadResponseMessage | SignupModifyResponseMessage;
 }
 
 export const SignupMessageValidator = SignupValidators.SignupMessageValidator;
